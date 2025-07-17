890b209f93c8e245765f9131f97ac2ca8fcf2633
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 13:34:48 +0200

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,13 +6,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Arminfrey\ContaoPersonenBundle\ContaoPersonenBundle
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(Arminfrey\ContaoPersonenBundle\ArminfreyContaoPersonenBundle::class)
+            BundleConfig::create(ContaoPersonenBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class])
         ];
     }
