2db3105b27f4a3b31a9e5c2af6568d2fc6adc00c
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 13:33:34 +0200

    Update ContaoPersonenBundle.php

--- src/ContaoPersonenBundle.php
+++ src/ContaoPersonenBundle.php
@@ -5,6 +5,6 @@ namespace Arminfrey\ContaoPersonenBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ArminfreyContaoPersonenBundle extends Bundle
+class ContaoPersonenBundle extends Bundle
 {
 }
