b5e6b161603a8b05e6b41e7a495d3a2ebc0e632c
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 11:47:10 +0200

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -9,6 +9,6 @@ ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['content'], 1, [
     'personen' => [
         'tables' => ['tl_personen'],
         'icon' => 'bundles/contaocore/images/modules/user.svg',
-        'print' => ['App\\Controller\\ContaoPersonenController', 'generatePrintList']
+        'print' => ['Arminfrey\\ContaoPersonenBundle\\Controller\\ContaoPersonenController', 'generatePrintList']
     ]
 ]);
