4d2572ad7dabb09e498f06b8a0ca224aaa644720
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 13:37:34 +0200

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,7 +6,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Arminfrey\ContaoPersonenBundle\ContaoPersonenBundle
+use Arminfrey\ContaoPersonenBundle\ContaoPersonenBundle;
 
 class Plugin implements BundlePluginInterface
 {
