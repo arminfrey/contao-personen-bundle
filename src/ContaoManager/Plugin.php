c2b0cba5db5bdc10cf664dd2993dd2e46d81a84b
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 10:25:06 +0200

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+// src/ContaoManager/Plugin.php
+namespace Arminfrey\PersonenBundle\ContaoManager;namespace Arminfrey\ContaoGbmBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use App\PersonenverwaltungBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(PersonenBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+        ];
+    }
+}
