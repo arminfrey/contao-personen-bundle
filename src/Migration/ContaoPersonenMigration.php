d1dc166982f27ab8bfa4e7388cf03b43ba2f504e
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 13:52:16 +0200

    Update ContaoPersonenMigration.php

--- src/Migration/ContaoPersonenMigration.php
+++ src/Migration/ContaoPersonenMigration.php
@@ -1,51 +1,58 @@
 <?php
 
 // src/Migration/ContaoPersonenMigration.php
+// src/Migration/PersonenMigration.php
 namespace Arminfrey\ContaoPersonenBundle\Migration;
 
 use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
 
 class PersonenMigration extends AbstractMigration
 {
-    private Connection $connection;
-
-    public function __construct(Connection $connection)
-    {
-        $this->connection = $connection;
-    }
+    public function __construct(
+        private Connection $connection
+    ) {}
 
     public function shouldRun(): bool
     {
         $schemaManager = $this->connection->createSchemaManager();
         
-        return !$schemaManager->tablesExist(['tl_personen']);
+        if (!$schemaManager->tablesExist(['tl_personen'])) {
+            return true;
+        }
+        
+        return false;
     }
 
     public function run(): MigrationResult
     {
-        $this->connection->executeStatement("
-            CREATE TABLE tl_personen (
-                id int(10) unsigned NOT NULL auto_increment,
-                tstamp int(10) unsigned NOT NULL default 0,
-                name1 varchar(255) NOT NULL default '',
-                vorname1 varchar(255) NOT NULL default '',
-                name2 varchar(255) NOT NULL default '',
-                vorname2 varchar(255) NOT NULL default '',
-                anrede varchar(10) NOT NULL default '',
-                strasse varchar(255) NOT NULL default '',
-                hausnummer varchar(20) NOT NULL default '',
-                plz varchar(10) NOT NULL default '',
-                ort varchar(255) NOT NULL default '',
-                email varchar(255) NOT NULL default '',
-                ansprache varchar(255) text NULL,
-                partnerfeld varchar(255) NOT NULL default '',
-                kategorie varchar(50) NOT NULL default '',
-                PRIMARY KEY (id)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-        ");
+        $schema = new Schema();
+        $table = $schema->createTable('tl_personen');
+        
+        $table->addColumn('id', 'integer', ['unsigned' => true, 'autoincrement' => true]);
+        $table->addColumn('tstamp', 'integer', ['unsigned' => true, 'default' => 0]);
+        $table->addColumn('name1', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('vorname1', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('name2', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('vorname2', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('anrede', 'string', ['length' => 10, 'default' => '']);
+        $table->addColumn('strasse', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('hausnummer', 'string', ['length' => 20, 'default' => '']);
+        $table->addColumn('plz', 'string', ['length' => 10, 'default' => '']);
+        $table->addColumn('ort', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('email', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('ansprache', 'text', ['notnull' => false]);
+        $table->addColumn('partnerfeld', 'string', ['length' => 255, 'default' => '']);
+        $table->addColumn('kategorie', 'string', ['length' => 50, 'default' => '']);
+        
+        $table->setPrimaryKey(['id']);
+        
+        foreach ($schema->toSql($this->connection->getDatabasePlatform()) as $sql) {
+            $this->connection->executeStatement($sql);
+        }
 
-        return $this->createResult(true);
+        return $this->createResult(true, 'Tabelle tl_personen wurde erfolgreich erstellt.');
     }
 }
