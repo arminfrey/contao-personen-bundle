7978560d97c539aeaf4e5c28435471b80463e5a6
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-17 11:23:38 +0200

    Create tl_personen.php
    initial

---
+++ contao/dca/languages/de/tl_personen.php
@@ -0,0 +1,28 @@
+<?php
+
+// contao/languages/de/tl_personen.php
+
+$GLOBALS['TL_LANG']['tl_personen']['name1'] = ['Nachname 1', 'Nachname der ersten Person'];
+$GLOBALS['TL_LANG']['tl_personen']['vorname1'] = ['Vorname 1', 'Vorname der ersten Person'];
+$GLOBALS['TL_LANG']['tl_personen']['name2'] = ['Nachname 2', 'Nachname der zweiten Person (optional)'];
+$GLOBALS['TL_LANG']['tl_personen']['vorname2'] = ['Vorname 2', 'Vorname der zweiten Person (optional)'];
+$GLOBALS['TL_LANG']['tl_personen']['anrede'] = ['Anrede', 'Anrede für die Person(en)'];
+$GLOBALS['TL_LANG']['tl_personen']['strasse'] = ['Straße', 'Straßenname'];
+$GLOBALS['TL_LANG']['tl_personen']['hausnummer'] = ['Hausnummer', 'Hausnummer inkl. Zusätze'];
+$GLOBALS['TL_LANG']['tl_personen']['plz'] = ['PLZ', 'Postleitzahl'];
+$GLOBALS['TL_LANG']['tl_personen']['ort'] = ['Ort', 'Ortsname'];
+$GLOBALS['TL_LANG']['tl_personen']['email'] = ['E-Mail', 'E-Mail-Adresse'];
+$GLOBALS['TL_LANG']['tl_personen']['ansprache'] = ['Ansprache', 'Persönliche Ansprache'];
+$GLOBALS['TL_LANG']['tl_personen']['partnerfeld'] = ['Partner', 'Partner-Information'];
+$GLOBALS['TL_LANG']['tl_personen']['kategorie'] = ['Kategorie', 'Kategorie der Person'];
+
+$GLOBALS['TL_LANG']['tl_personen']['person_legend'] = 'Personen-Daten';
+$GLOBALS['TL_LANG']['tl_personen']['address_legend'] = 'Adresse';
+$GLOBALS['TL_LANG']['tl_personen']['contact_legend'] = 'Kontakt';
+$GLOBALS['TL_LANG']['tl_personen']['category_legend'] = 'Kategorie';
+
+$GLOBALS['TL_LANG']['tl_personen']['new'] = ['Neue Person', 'Eine neue Person anlegen'];
+$GLOBALS['TL_LANG']['tl_personen']['edit'] = ['Person bearbeiten', 'Person ID %s bearbeiten'];
+$GLOBALS['TL_LANG']['tl_personen']['copy'] = ['Person kopieren', 'Person ID %s kopieren'];
+$GLOBALS['TL_LANG']['tl_personen']['delete'] = ['Person löschen', 'Person ID %s löschen'];
+$GLOBALS['TL_LANG']['tl_personen']['show'] = ['Person anzeigen', 'Person ID %s anzeigen'];
